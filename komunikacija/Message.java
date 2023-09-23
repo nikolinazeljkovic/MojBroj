3921da2225dee9b93642dee9ccaab780691f4483
Nikolina <102320119+nikolinazeljkovic@users.noreply.github.com>
2023-09-23 21:16:43 +0200

    Update Message.java

--- komunikacija/Message.java
+++ komunikacija/Message.java
@@ -1,8 +1,20 @@
 
-public class Message
-{
+public class Message {
+	
+  public static final String SEPARATOR = "::";
   private String header;
-	private String value;
+  private String value;
   
   public Message() {}
+
+  public Message(String header, String value) {
+        super();
+	this.header = header;
+	this.value = value;
+	}
+
+  @Override
+  public String toString() {
+       return header + SEPARATOR + value;
+	}
 }
