275459cd334fcd7d2632c273119f441045d21396
Nikolina <102320119+nikolinazeljkovic@users.noreply.github.com>
2023-09-23 21:09:48 +0200

    Update ExpressionMessage.java

--- komunikacija/ExpressionMessage.java
+++ komunikacija/ExpressionMessage.java
@@ -1,1 +1,24 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+public class ExpressionMessage
+{
+  private List<Integer> numbers;
+	private int result;
+	private LocalDateTime startDateTime;// treba start date time samo za prvu rundu
+	private long roundDurationInSeconds;
+	public ExpressionMessage() {
+		super();
+	}
+
+  public ExpressionMessage(List<Integer> numbers, int result, LocalDateTime startDateTime, long roundDurationInSeconds)
+  {
+    super();
+		this.numbers = numbers;
+		this.result = result;
+		this.startDateTime = startDateTime;
+		this.roundDurationInSeconds = roundDurationInSeconds;
+  }
+}
