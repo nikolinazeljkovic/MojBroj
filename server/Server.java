07ae0208511a3f58bdcd7e0592c169bdee0ac2c5
Nikolina <102320119+nikolinazeljkovic@users.noreply.github.com>
2023-09-23 21:04:43 +0200

    Update Server.java

--- server/Server.java
+++ server/Server.java
@@ -1,1 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
 
+import server.model.Player;
+
+public class Server
+{
+  public static final int PORT = 9996;
+	private static Player waitingPlayer = null;
+	private static Set<Player> connectedPlayers = new HashSet<Player>();
+    
+}
