fdafbc61f190dd47c4b1d70c8dc3d0efc85e6cad
Stefan Popovic <110554536+stefanpopovic314@users.noreply.github.com>
2023-09-25 08:15:34 +0200

    Update Server.java

--- server/Server.java
+++ server/Server.java
@@ -8,5 +8,21 @@ public class Server
   public static final int PORT = 9996;
 	private static Player waitingPlayer = null;
 	private static Set<Player> connectedPlayers = new HashSet<Player>();
-    
+
+public static synchronized boolean addPlayer(Player player) {
+		return connectedPlayers.add(player);
+	}
+
+
+//TODO main
+
+private static String getName(String message) throws MessageException {
+		Message m = Message.parseMessage(message);
+		
+		if (Header.NAME.getValue().equals(m.getHeader()))
+			return m.getValue();
+		
+		throw new RuntimeException("Name is not sent");
+	}
+	
 }
