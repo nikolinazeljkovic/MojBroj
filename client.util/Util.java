5ef1aed9743ff8fd59f18f011ab284d8414464e5
Nikolina <102320119+nikolinazeljkovic@users.noreply.github.com>
2023-09-25 22:41:47 +0200

    Update Util.java

--- client.util/Util.java
+++ client.util/Util.java
@@ -1,1 +1,10 @@
+package client.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Util 
+{
+  private static final Pattern EXPR_RE = Pattern.compile("\\G\\s*([+-]?)\\s*([^+-]+)");
+	private static final Pattern TERM_RE = Pattern.compile("\\G(^|(?<!^)\\*|(?<!^)/)\\s*(\\d*\\.?\\d+)\\s*");
+}
