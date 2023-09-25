0cc151dc753db7a362171809f4307be6133aa083
Nikolina <102320119+nikolinazeljkovic@users.noreply.github.com>
2023-09-25 22:34:03 +0200

    Update MessageException.java

--- komunikacija/MessageException.java
+++ komunikacija/MessageException.java
@@ -1,1 +1,13 @@
+package komunikacija;
 
+@SuppressWarnings("serial")
+public class MessageException extends Exception
+{
+	public MessageException() {
+		super();
+	}
+
+	public MessageException(String message) {
+		super(message);
+	}
+}
