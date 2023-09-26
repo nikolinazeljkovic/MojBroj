00188e722532ad48a8d3981d7ff5f8cc1bce0a10
Stefan Popovic <110554536+stefanpopovic314@users.noreply.github.com>
2023-09-26 07:56:11 +0200

    Update Util.java

--- client.util/Util.java
+++ client.util/Util.java
@@ -8,3 +8,28 @@ public class Util
   private static final Pattern EXPR_RE = Pattern.compile("\\G\\s*([+-]?)\\s*([^+-]+)");
 	private static final Pattern TERM_RE = Pattern.compile("\\G(^|(?<!^)\\*|(?<!^)/)\\s*(\\d*\\.?\\d+)\\s*");
 }
+
+private static double evalTerm(String term) {
+		Matcher m = TERM_RE.matcher(term);
+		double product = Double.NaN;
+		int matchEnd;
+		for (matchEnd = -1; m.find(); matchEnd = m.end()) {
+			switch (m.group(1)) {
+			case "*":
+				product *= Integer.parseInt(m.group(2));
+				break;
+			case "/":
+				product /= Integer.parseInt(m.group(2));
+				break;
+			case "":
+				product = Integer.parseInt(m.group(2));
+				break;
+			}
+		}
+		if (matchEnd != term.length()) {
+			throw new IllegalArgumentException("Invalid term \"" + term + "\"");
+		}
+		return product;
+}
+
+//TODO CALCULATE
