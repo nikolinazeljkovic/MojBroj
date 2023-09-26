2379ec900bcbe41cc5900d134fdfa1016d0a2400
Stefan Popovic <110554536+stefanpopovic314@users.noreply.github.com>
2023-09-26 08:17:22 +0200

    Update Util.java

--- client.util/Util.java
+++ client.util/Util.java
@@ -7,7 +7,6 @@ public class Util
 {
   private static final Pattern EXPR_RE = Pattern.compile("\\G\\s*([+-]?)\\s*([^+-]+)");
 	private static final Pattern TERM_RE = Pattern.compile("\\G(^|(?<!^)\\*|(?<!^)/)\\s*(\\d*\\.?\\d+)\\s*");
-}
 
 private static double evalTerm(String term) {
 		Matcher m = TERM_RE.matcher(term);
@@ -32,4 +31,17 @@ private static double evalTerm(String term) {
 		return product;
 }
 
-//TODO CALCULATE
+	public static int calculate(String expr) {   // racuna izzracuna izraz iz stringa , koristi donju funkciju
+		Matcher m = EXPR_RE.matcher(expr);
+		int sum = 0;
+		int matchEnd;
+		for (matchEnd = -1; m.find(); matchEnd = m.end()) {
+			sum += (("-".equals(m.group(1))) ? -1 : +1) * evalTerm(m.group(2));
+		}
+		if (matchEnd != expr.length()) {
+			throw new IllegalArgumentException("Invalid expression \"" + expr + "\"");
+		}
+		return sum;
+	}
+}
+
