28e7b2e04a2d7195bd60d78a01a52028efc09f12
Stefan Popovic <110554536+stefanpopovic314@users.noreply.github.com>
2023-09-26 08:14:15 +0200

    Update Round.java

--- server/server.model/Round.java
+++ server/server.model/Round.java
@@ -1,1 +1,15 @@
+package server.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Round {
+	public static final long DURATION_SECONDS = 60L;
+	private Expression expression;
+	private Map<Player, Integer> playersPoints;//RACUNANJE BODOVA
+
+	public Round() {
+		this.expression = new Expression();
+		this.playersPoints = new HashMap<Player, Integer>();
+	}
+//TODO
