84b00fd1038cb4bf679500376e221942f63ccbf7
Nikolina <102320119+nikolinazeljkovic@users.noreply.github.com>
2023-09-28 19:41:35 +0200

    Update Expression.java

--- server/server.model/Expression.java
+++ server/server.model/Expression.java
@@ -85,4 +85,30 @@ public class Expression {
 	public String toString() {
 		return expression;
 	}
+
+	private static final Pattern EXPR_RE = Pattern.compile("\\G\\s*([+-]?)\\s*([^+-]+)");
+	private static final Pattern TERM_RE = Pattern.compile("\\G(^|(?<!^)\\*|(?<!^)/)\\s*(\\d*\\.?\\d+)\\s*");
+
+	private static double evalTerm(String term) {
+		Matcher m = TERM_RE.matcher(term);
+		double product = Double.NaN;
+		int matchEnd;
+		for (matchEnd = -1; m.find(); matchEnd = m.end()) {
+			switch (m.group(1)) {
+			case "*":
+				product *= Integer.parseInt(m.group(2));
+				break;
+			case "/":
+				product /= Integer.parseInt(m.group(2));
+				break;
+			case "":
+				product = Integer.parseInt(m.group(2));
+				break;
+			}
+		}
+		if (matchEnd != term.length()) {
+			throw new IllegalArgumentException("Invalid term \"" + term + "\"");
+		}
+		return product;
+	}
 }
